591d2fe16b2f0e036b163706ee8e1933e70cc7c3
triple-ones-wonder <a15612469451@163.com>
2026-06-16 21:42:25 +0800

    fix: MP3解码后添加Convert_Stereo/Convert_Mono调用，修复噪音；I2S时钟动态同步MP3采样率，修复播放速度异常
    - mp3_play.c: Convert_Stereo/Convert_Mono 添加nSamps参数，支持可变帧大小(MPEG1/MPEG2)
    - main.c: 每次MP3Decode后调用对应Convert函数，消除噪音
    - main.c: 取消注释I2S重配置代码，首帧解码后自动同步采样率
    - i2s.c: CubeMX默认I2S频率从44K改为48K

--- Drivers/STM32_F103_Drivers/helixv_MP3/mp3_play.c
+++ Drivers/STM32_F103_Drivers/helixv_MP3/mp3_play.c
@@ -27,20 +27,21 @@
 * Description    : I have do some modification in Subband() function due to the long time of
 *                  decode.Using PolyphaseMono() function which is used in decode mono mp3,to decode
 *                  Stereo,so must use this funtion to convert mono to Stereo.
-* Input          : Adress of buffer data
+* Input          : Adress of buffer data, number of input samples (outputSamps from decoder)
 * Output         : None
 * Return         : None
 */
 /************************************************************************************/
-void Convert_Stereo(short *buffer)
+void Convert_Stereo(short *buffer, int nSamps)
 {
-	int i,j,k=0;
-	for(i=0;i<(2304/64);i++)
+	int i,j,k;
+	int nBlocks = nSamps / 64;  /* MPEG1: 2304/64=36, MPEG2: 1152/64=18 */
+	for(i=0; i<nBlocks; i++)
 	{
-		for(j=31+i*64,k=j+32;j>=0+i*64;j--,k-=2)
+		for(j=31+i*64, k=j+32; j>=0+i*64; j--, k-=2)
 		{
 			buffer[k]=buffer[j];
-			buffer[k-1]=buffer[j]; 
+			buffer[k-1]=buffer[j];
 		}
 	}
 }
@@ -48,17 +49,17 @@ void Convert_Stereo(short *buffer)
 /***********************************************************************************/
 /*
 * File Name  : Convert_Mono
-* Description    : after decoder a frame of Mono MP3,the buffer will fill by 1152 halfword,this function
+* Description    : after decoder a frame of Mono MP3,the buffer will fill by nSamps halfword,this function
 *                   will change  Mono into Stereo.  Mono:L1,L2,L3,L4.  Stereo:L1R1,L2R2,L3R3.L4R4
-* Input          : Adress of buffer data
+* Input          : Adress of buffer data, number of input samples (outputSamps from decoder)
 * Output         : None
 * Return         : None
 */
 /***********************************************************************************/
-void Convert_Mono(short *buffer)
+void Convert_Mono(short *buffer, int nSamps)
 {
 	int i;
-	for (i = 1152 - 1; i >= 0; i--)
+	for (i = nSamps - 1; i >= 0; i--)
 	{
 		buffer[i * 2] = buffer[i];
 		buffer[i * 2 + 1] = buffer[i];
